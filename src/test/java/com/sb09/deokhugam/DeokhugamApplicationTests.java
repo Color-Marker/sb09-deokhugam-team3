fcd9197c9dbfb5f74d3cbe171969129c3067040f
Color-Marker <AAAstudying0987@gmail.com>
2026-04-13 14:49:34 +0900

    세팅: 초기 프로젝트 세팅

---
+++ src/test/java/com/sb09/deokhugam/DeokhugamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb09.deokhugam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokhugamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
