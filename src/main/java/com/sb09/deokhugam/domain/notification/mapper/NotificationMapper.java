49e137133c0b6c8dba149bd0ee81a4a34d3f4eaf
Color-Marker <AAAstudying0987@gmail.com>
2026-04-15 10:17:06 +0900

    개발: 페이지네이션 제외 컨트롤러에서 요구하는 알람 기능 추가 완료

--- src/main/java/com/sb09/deokhugam/domain/notification/mapper/NotificationMapper.java
+++ src/main/java/com/sb09/deokhugam/domain/notification/mapper/NotificationMapper.java
@@ -1,9 +1,18 @@
 package com.sb09.deokhugam.domain.notification.mapper;
 
 import com.sb09.deokhugam.domain.notification.dto.response.NotificationDto;
+import com.sb09.deokhugam.domain.notification.entity.Notification;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public class NotificationMapper {
-  NotificationDto toDto();
+public interface NotificationMapper {
+  @Mapping(target = "id", source = "id")
+  @Mapping(target = "userId", source = "user.id")
+  @Mapping(target = "reviewId", source = "review.id")
+  @Mapping(target = "reviewContent", source = "review.content")
+  @Mapping(target = "confirmed", source = "confirmStatus")
+  @Mapping(target = "message", source = "message")
+  NotificationDto toDto(Notification notification, String message);
 }
--- src/main/java/com/sb09/deokhugam/domain/notification/service/basic/BasicNotificationService.java
+++ src/main/java/com/sb09/deokhugam/domain/notification/service/basic/BasicNotificationService.java
@@ -4,11 +4,16 @@ import com.sb09.deokhugam.domain.notification.dto.request.NotificationListReques
 import com.sb09.deokhugam.domain.notification.dto.request.NotificationUpdateRequest;
 import com.sb09.deokhugam.domain.notification.dto.response.NotificationDto;
 import com.sb09.deokhugam.domain.notification.entity.Notification;
+import com.sb09.deokhugam.domain.notification.entity.NotificationType;
 import com.sb09.deokhugam.domain.notification.mapper.NotificationMapper;
 import com.sb09.deokhugam.domain.notification.repository.NotificationRepository;
 import com.sb09.deokhugam.domain.notification.service.NotificationService;
+import com.sb09.deokhugam.domain.user.entity.Users;
+import com.sb09.deokhugam.global.Exception.CustomException;
+import com.sb09.deokhugam.global.Exception.ErrorCode;
 import com.sb09.deokhugam.global.common.dto.CursorPageResponseDto;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,30 +27,70 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class BasicNotificationService implements NotificationService {
   private final NotificationRepository notificationRepository;
+  private final UserRepository userRepository;
   private final NotificationMapper notificationMapper;
 
   @Override
   public ResponseEntity<Void> readAll(UUID userId) {
     if(userId == null){
-      log.warn("사용자 ID가 입력되지 않았습니다.");
-
+      log.warn("잘못된 요청입니다.");
+      throw new CustomException(ErrorCode.INVALID_REQUEST);
+    }
+    if(!userRepository.existsById(userId)){
+      log.warn("사용자를 찾을 수 없습니다.");
+      throw new CustomException(ErrorCode.USER_NOT_FOUND);
     }
     List<Notification> notis = notificationRepository.findAllByUserId(userId);
     for (Notification n : notis){
       n.update();
     }
-    return null;
+    return ResponseEntity.noContent().build();
   }
 
   @Override
-  public NotificationDto updateStatus(UUID notificationId, UUID userId,
-      NotificationUpdateRequest request) {
+  public NotificationDto updateStatus(UUID notificationId, UUID userId, NotificationUpdateRequest request) {
+    if(userId == null){
+      log.warn("잘못된 요청입니다.");
+      throw new CustomException(ErrorCode.INVALID_REQUEST);
+    }
 
-    return null;
+    Users user = userRepository.findById(userId);
+    Notification notification = notificationRepository.findById(notificationId).
+        orElseThrow(() -> {
+          log.warn("알림을 찾을 수 없습니다");
+          return new CustomException(ErrorCode.NOTIFICATION_NOT_FOUND);
+        });
+
+    if(!notification.getUser().equals(user)){
+      log.warn("알림에 대한 접근 권한이 없습니다.");
+      throw new CustomException(ErrorCode.NOTIFICATION_ACCESS_FORBIDDEN);
+    }
+
+    notification.update();
+    String message;
+
+    if(notification.getType().equals(NotificationType.RANKING)){
+      message = "당신의 리뷰가 인기 순위 10위 내에 선정되었습니다.";
+    }
+    else {
+      String nickname = notification.getSender() != null ? notification.getSender().getNickname() : "알 수 없음";
+      message = createMessage(notification.getType(), nickname);
+    }
+
+    return notificationMapper.toDto(notification, message);
   }
 
   @Override
   public CursorPageResponseDto<NotificationDto> list(NotificationListRequest request) {
     return null;
+  }
+
+  public String createMessage(NotificationType type, String nickname){
+    if(type.equals(NotificationType.LIKE)) {
+      return "[" + nickname + "]님이 나의 리뷰를 좋아합니다.";
+    }
+    else {
+      return "[" + nickname + "]님이 나의 리뷰에 댓글을 남겼습니다.";
+    }
   }
 }
