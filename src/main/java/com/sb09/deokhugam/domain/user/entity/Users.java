a04dba90976a3249289286430fc214646cd91a58
Color-Marker <AAAstudying0987@gmail.com>
2026-04-16 09:32:10 +0900

    환경: application yaml 및 schema.sql과의 연결 시도

--- src/main/java/com/sb09/deokhugam/domain/user/entity/Users.java
+++ src/main/java/com/sb09/deokhugam/domain/user/entity/Users.java
@@ -24,16 +24,6 @@ public class Users extends BaseFullAuditEntity {
   @Column(nullable = false, length = 255)
   private String password;
 
-  // 파워 유저 랭킹 집계를 위한 필드
-  @Column(name = "review_score_sum", nullable = false)
-  private Double reviewScoreSum = 0.0;
-
-  @Column(name = "like_count", nullable = false)
-  private Long likeCount = 0L;
-
-  @Column(name = "comment_count", nullable = false)
-  private Long commentCount = 0L;
-
   @Builder
   public Users(String email, String nickname, String password) {
     this.email = email;
@@ -43,29 +33,5 @@ public class Users extends BaseFullAuditEntity {
 
   public void updateNickname(String nickname) {
     this.nickname = nickname;
-  }
-
-  public void increaseCommentCount() {
-    this.commentCount++;
-  }
-
-  public void decreaseCommentCount() {
-    if (this.commentCount > 0) {
-      this.commentCount--;
-    }
-  }
-
-  public void increaseLikeCount() {
-    this.likeCount++;
-  }
-
-  public void decreaseLikeCount() {
-    if (this.likeCount > 0) {
-      this.likeCount--;
-    }
-  }
-
-  public void addReviewScore(Double score) {
-    this.reviewScoreSum += score;
   }
 }
