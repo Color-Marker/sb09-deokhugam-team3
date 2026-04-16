ecdfb12855111c454ab1190d39b8d29e13379b11
geumbi <gkrry0404@naver.com>
2026-04-17 03:54:27 +0900

    개발: 서비스 수정 및 컨트롤러 생성 완료2

--- src/main/java/com/sb09/deokhugam/domain/user/service/basic/UserService.java
+++ src/main/java/com/sb09/deokhugam/domain/user/service/basic/UserService.java
@@ -1,4 +1,4 @@
-package com.sb09.deokhugam.domain.user.service.impl;
+package com.sb09.deokhugam.domain.user.service.basic;
 
 import com.sb09.deokhugam.domain.user.dto.Response.UserResponse;
 import com.sb09.deokhugam.domain.user.dto.request.UserLoginRequest;
@@ -7,7 +7,6 @@ import com.sb09.deokhugam.domain.user.dto.request.UserUpdateRequest;
 import com.sb09.deokhugam.domain.user.entity.Users;
 import com.sb09.deokhugam.domain.user.mapper.UserMapper;
 import com.sb09.deokhugam.domain.user.repository.UserRepository;
-import com.sb09.deokhugam.domain.user.service.UserService;
 import com.sb09.deokhugam.global.Exception.CustomException;
 import com.sb09.deokhugam.global.Exception.ErrorCode;
 import java.util.UUID;
@@ -20,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserService implements com.sb09.deokhugam.domain.user.service.UserService {
 
   private final UserRepository userRepository;
   private final UserMapper userMapper;
