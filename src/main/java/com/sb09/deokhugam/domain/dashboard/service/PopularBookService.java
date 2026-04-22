5b63e2de1b270014a955f040a80ec2c0d05eb0b1
Color-Marker <AAAstudying0987@gmail.com>
2026-04-22 09:48:52 +0900

    수정: dto 제거

--- src/main/java/com/sb09/deokhugam/domain/dashboard/service/PopularBookService.java
+++ src/main/java/com/sb09/deokhugam/domain/dashboard/service/PopularBookService.java
@@ -1,8 +1,5 @@
 package com.sb09.deokhugam.domain.dashboard.service;
 
-import com.sb09.deokhugam.domain.dashboard.dto.request.PopularBookListRequest;
-import com.sb09.deokhugam.domain.dashboard.dto.response.PopularBookDto;
-import com.sb09.deokhugam.global.common.dto.CursorPageResponseDto;
 import java.time.LocalDate;
 
 public interface PopularBookService {
