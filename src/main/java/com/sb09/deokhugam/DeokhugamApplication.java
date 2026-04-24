dbc52b5d7749a6a474ae19b3afdf292211632cb2
apple <kjw010900@gmail.com>
2026-04-24 10:08:07 +0900

    수정: 테스트 profile을 더미데이터로 받기위한 어노테이션 추

--- src/main/java/com/sb09/deokhugam/DeokhugamApplication.java
+++ src/main/java/com/sb09/deokhugam/DeokhugamApplication.java
@@ -3,7 +3,6 @@ package com.sb09.deokhugam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableScheduling
 @SpringBootApplication
