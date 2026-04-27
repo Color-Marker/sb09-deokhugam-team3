683d525a3fe8518669a14c080697ae05cf6678d5
전명훈 <acekid2120@gmail.com>
2026-04-27 11:26:20 +0900

    수정: 'Delete & Insert' 방식 추가

--- src/main/java/com/sb09/deokhugam/domain/dashboard/repository/PopularBookRepository.java
+++ src/main/java/com/sb09/deokhugam/domain/dashboard/repository/PopularBookRepository.java
@@ -11,5 +11,8 @@ public interface PopularBookRepository extends JpaRepository<PopularBook, UUID> 
 
   //특정 period의 가장 최신 base_date조회 (최신 배치 결과 기준)
   Optional<PopularBook> findTopByPeriodOrderByBaseDateDesc(PeriodType period);
+
   boolean existsByBaseDate(LocalDate baseDate);
+
+  void deleteByPeriodAndBaseDate(PeriodType period, LocalDate baseDate);
 }
