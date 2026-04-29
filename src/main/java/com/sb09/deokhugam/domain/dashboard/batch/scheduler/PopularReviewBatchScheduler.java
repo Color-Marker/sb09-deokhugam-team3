06d7f0a6dd47f6e64e710c81857c21eec189dfe9
전명훈 <acekid2120@gmail.com>
2026-04-29 11:07:33 +0900

    개발: config, 스케쥴러 코드 작성

---
+++ src/main/java/com/sb09/deokhugam/domain/dashboard/batch/scheduler/PopularReviewBatchScheduler.java
@@ -0,0 +1,30 @@
+package com.sb09.deokhugam.domain.dashboard.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PopularReviewBatchScheduler {
+  private final JobLauncher jobLauncher;
+  private final Job calculatePopularReviewJob;
+
+  @Scheduled(cron = "0 5 0 * * *") // 매일 00시 05분 실행
+  public void calculatePopularReview(){
+    try{
+      JobParameters jobParameters = new JobParametersBuilder()
+          .addLong("time", System.currentTimeMillis())
+          .toJobParameters();
+      jobLauncher.run(calculatePopularReviewJob, jobParameters);
+    } catch (Exception e){
+      log.error("인기 리뷰 배치 작업 중 오류가 발생하였습니다. 세부 내용: {}", e.getMessage());
+    }
+  }
+}
---
+++ src/main/java/com/sb09/deokhugam/domain/dashboard/batch/scheduler/PowerUserBatchScheduler.java
@@ -0,0 +1,30 @@
+package com.sb09.deokhugam.domain.dashboard.batch.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PowerUserBatchScheduler {
+  private final JobLauncher jobLauncher;
+  private final Job calculatePowerUserJob;
+
+  @Scheduled(cron = "0 10 0 * * *") // 매일 00시 10분 실행
+  public void calculatePowerUser(){
+    try{
+      JobParameters jobParameters = new JobParametersBuilder()
+          .addLong("time", System.currentTimeMillis())
+          .toJobParameters();
+      jobLauncher.run(calculatePowerUserJob, jobParameters);
+    } catch (Exception e){
+      log.error("파워 유저 배치 작업 중 오류가 발생하였습니다. 세부 내용: {}", e.getMessage());
+    }
+  }
+}
