4042f77a1dce64cdf4dd5e81f441409002900d0d
geumbi <gkrry0404@naver.com>
2026-05-05 19:59:13 +0900

    수정: Swagger 예시값 난수 생성 문제 수정

--- src/main/java/com/sb09/deokhugam/domain/user/dto/request/UserRegisterRequest.java
+++ src/main/java/com/sb09/deokhugam/domain/user/dto/request/UserRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.sb09.deokhugam.domain.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -7,16 +8,19 @@ import jakarta.validation.constraints.Size;
 
 public record UserRegisterRequest(
 
+    @Schema(example = "user@example.com")
     @NotBlank(message = "이메일은 필수 입력값입니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
         message = "이메일 형식이 올바르지 않습니다.")
     String email,
 
+    @Schema(example = "홍길동")
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
     String nickname,
 
+    @Schema(example = "Password123!")
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
         message = "비밀번호는 8~20자리이면서 영문, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다.")
